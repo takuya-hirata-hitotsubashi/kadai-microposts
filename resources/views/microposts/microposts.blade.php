56060fee55f2ba0c4436f21bbd866171f52bcaef
takuya-hirata-hitotsubashi <ec2-user@ip-172-31-3-238.us-east-2.compute.internal>
2018-03-16 05:01:04 +0000

    follow unfollow

--- resources/views/microposts/microposts.blade.php
+++ resources/views/microposts/microposts.blade.php
@@ -12,6 +12,13 @@
             <div>
                 <p>{!! nl2br(e($micropost->content)) !!}</p>
             </div>
+            <div>
+                @if (Auth::user()->id == $micropost->user_id)
+                    {!! Form::open(['route' => ['microposts.destroy', $micropost->id], 'method' => 'delete']) !!}
+                        {!! Form::submit('Delete', ['class' => 'btn btn-danger btn-xs']) !!}
+                    {!! Form::close() !!}
+                @endif
+            </div>
         </div>
     </li>
 @endforeach
